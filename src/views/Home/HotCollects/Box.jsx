1c413e7c03de284d405b2f272bc651ef8ef3b48f
xxl <870161348@qq.com>
2021-04-20 22:35:31 +0800

    热门收藏品

--- src/views/Home/HotCollects/Box.jsx
+++ src/views/Home/HotCollects/Box.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+import Item from "./Item";
+
+import hot1 from "image/home/collects/hot1.png";
+import hot2 from "image/home/collects/hot2.png";
+import hot3 from "image/home/collects/hot3.png";
+import hot4 from "image/home/collects/hot4.png";
+import artist1 from "image/home/collects/artist1.png";
+import artist2 from "image/home/collects/artist2.png";
+import artist3 from "image/home/collects/artist3.png";
+import artist4 from "image/home/collects/artist4.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
 }));
-
+const data = [
+  ["层波、逆流与浪", "0.002", "艺术品-加密意识", "王牧羽", hot1, artist1],
+  ["预言", "0.002", "艺术品-加密艺术", "景晓雷", hot2, artist2],
+  ["财神", "0.002", "艺术品-加密艺术", "李婷婷", hot3, artist3],
+  ["墙-赭石", "0.002", "艺术品-加密意识", "于洋", hot4, artist4],
+];
 export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
+        {data.map((value) => (
+          <Grid item xs={6} sm={3}>
+            <Item item={value} />
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
--- src/views/Home/HotCollects/Item.jsx
+++ src/views/Home/HotCollects/Item.jsx
@@ -1,20 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,103 +13,104 @@ const useStyles = makeStyles((theme) => ({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "100%", // 16:9
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+  spec: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
-  expandOpen: {
-    transform: 'rotate(180deg)',
+  spec2: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: theme.spacing(1),
   },
-  avatar: {
-    backgroundColor: red[500],
+  subtitle: {
+    fontSize: 15,
+    fontFamily: "PingFangSC-Medium, PingFang SC",
+    fontWeight: 500,
+    color: "#000000",
+  },
+  specText: {
+    fontSize: 9,
+    fontFamily: "PingFangSC-Medium, PingFang SC",
+    fontWeight: 400,
+    color: "#626262",
+  },
+  bottom: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: -theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  user: {
+    fontSize: 10,
+    fontFamily: "PingFangSC-Medium, PingFang SC",
+    fontWeight: 400,
   },
 }));
 
-export default function RecipeReviewCard() {
+export default function RecipeReviewCard({ item }) {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
+      <div style={{ height: 10, backgroundColor: "#E8D252" }}></div>
       <CardMedia
         className={classes.media}
-        image="/static/images/cards/paella.jpg"
+        image={item[4]}
         title="Paella dish"
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
+        <div className={classes.spec}>
+          <Typography variant="h6" component="p" className={classes.subtitle}>
+            {item[0]}
+          </Typography>
+          <Typography variant="h6" component="p" className={classes.subtitle}>
+            Ξ {item[1]}
+          </Typography>
+        </div>
+        <div className={classes.spec2}>
+          <Typography variant="h6" component="p" className={classes.specText}>
+            {item[2]}
+          </Typography>
+          <Typography variant="h6" component="p" className={classes.specText}>
+            价格
+          </Typography>
+        </div>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
+      <Divider
+        style={{
+          marginLeft: "5%",
+          width: "90%",
+          marginTop: 25,
+          marginBottom: 35,
+        }}
+      />
+      <CardActions disableSpacing style={{ marginBottom: 24 }}>
+        <img src={item[5]} alt="" />
+        <div className={classes.bottom}>
+          <Typography
+            className={classes.user}
+            style={{
+              color: "#626262",
+              padding: 8,
+            }}
+          >
+            ARTIST
+          </Typography>
+          <Typography
+            className={classes.user}
+            style={{
+              paddingLeft: 8,
+            }}
+          >
+            {item[3]}
+          </Typography>
+        </div>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
